46618b3881a6793861c135c2f40c98a10ad28ddb
dannamishelle23 <dannamishelle.23@gmail.com>
2025-07-26 12:45:16 -0500

    interfaz corregida y mejorada

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-#Importar el archivo autenticacion.py
+#1. Importar el archivo autenticacion.py
 import autenticacion  
 
-#Cargar modelo y utilidades
+#2. Cargar modelo y utilidades
 modelo = load_model("chatbot_modelo.keras")
 with open("tokenizer.pickle", "rb") as handle:
     tokenizer = pickle.load(handle)
@@ -18,7 +18,7 @@ with open("labels.pickle", "rb") as enc:
 with open("intents.json", encoding="utf-8") as file:
     intents = json.load(file)
 
-#Inicializar estados
+#3. Inicializar estados
 if "estado" not in st.session_state:
     st.session_state.estado = "auth" 
 
@@ -34,6 +34,7 @@ if "nombre_usuario" not in st.session_state:
 if "chat" not in st.session_state:
     st.session_state.chat = []
 
+#4. Funcion para obtener la respuesta que el usuario escriba en el chat
 def obtener_respuesta(texto_usuario):
     secuencia = tokenizer.texts_to_sequences([texto_usuario])
     padded = pad_sequences(secuencia, maxlen=modelo.input_shape[1], padding='post')
@@ -45,41 +46,61 @@ def obtener_respuesta(texto_usuario):
             return random.choice(intent["responses"])
     return "Lo siento, no entiendo tu pregunta. ¿Podrías ser más específico?"
 
+#Añadir un titulo a la interfaz
 st.title("Plataforma de Tutorías ESFOT")
 
+#5. Mostrar la interfaz para que el usuario inicie sesion o se registre
 if st.session_state.estado == "auth":
     opcion = st.selectbox("Selecciona una opción:", ["Iniciar sesión", "Registrarse"])
 
-    correo = st.text_input("Correo")
-    password = st.text_input("Contraseña", type="password")
-
+    #Si el usuario elige "Registro"
     if opcion == "Registrarse":
         nombre = st.text_input("Nombre completo")
-        rol = st.selectbox("Rol", ["estudiante", "docente", "administrador"])
+        correo = st.text_input("Correo")
+        password = st.text_input("Contraseña", type="password")
+        rol = st.selectbox("Rol", ["Estudiante", "Docente", "Administrador"])
         if st.button("Registrarse"):
-            if autenticacion.registrar_usuario(correo, password, rol, nombre):
-                st.success("Cuenta registrada! Ahora puedes iniciar sesión.")
+            if not nombre or not correo or not password or not rol:
+                st.warning("Todos los campos son obligatorios!")
+            elif "@" not in correo or "." not in correo:
+                st.warning("Introduce un correo electrónico válido.")
             else:
-                st.error("Error al registrar. Verifica los datos o el correo ya existe.")
+                respuesta = autenticacion.registrar_usuario(nombre, correo, password, rol)
+                if not respuesta["error"]:
+                    st.success("Cuenta registrada! Ahora puedes iniciar sesión.")
+                    st.session_state.estado = "auth"
+                else:
+                    if "usuario_existente" in respuesta:
+                        u = respuesta["usuario_existente"]
+                        st.error(f"Ya existe una cuenta registrada con ese correo ({u['correo']}) con rol de **{u['rol']}**.")
+                    else:
+                        st.error("No se pudo registrar. Verifica los campos.")
 
+    #Si el usuario ya tiene cuenta creada
     elif opcion == "Iniciar sesión":
+        correo = st.text_input("Correo")
+        password = st.text_input("Contraseña", type="password")
         if st.button("Iniciar sesión"):
-            usuario = autenticacion.login_usuario(correo, password)
-            if usuario:
-                st.success(f"Has iniciado sesión con éxito, {usuario['nombre']} ({usuario['rol']})")
-                st.session_state.estado = "chatbot"
-                st.session_state.correo_usuario = correo
-                st.session_state.rol_usuario = usuario['rol']
-                st.session_state.nombre_usuario = usuario['nombre']
-                st.session_state.chat = [
-            ("Bot", f"Hola {usuario['nombre']}, bienvenido a la plataforma de tutorías de la ESFOT. ¿En qué puedo ayudarte?")
-        ]
+            if not correo or not password:
+                st.warning("Completa todos los campos para iniciar sesión.")
             else:
-                st.error("Correo o contraseña incorrectos.")
+                usuario = autenticacion.login_usuario(correo, password)
+                if usuario:
+                    st.success(f"Has iniciado sesión con éxito, {usuario['nombre']} ({usuario['rol']})")
+                    st.session_state.estado = "chatbot"
+                    st.session_state.nombre_usuario = usuario['nombre']
+                    st.session_state.correo_usuario = correo
+                    st.session_state.rol_usuario = usuario['rol']
+                    st.session_state.chat = [
+                ("Bot", f"Hola {usuario['nombre']}, bienvenido a la plataforma de tutorías de la ESFOT. ¿En qué puedo ayudarte?")
+            ]
+                else:
+                    st.error("Correo o contraseña incorrectos.")
 
+#6. Mostrar al usuario la interfaz del chatbot luego de iniciar sesion
 elif st.session_state.estado == "chatbot":
-    st.write(f"Usuario: **{st.session_state.nombre_usuario}** | Rol: **{st.session_state.rol_usuario}**")
-
+    st.write(f"Usuario: **{st.session_state.nombre_usuario}**")
+    st.write(f"Rol: **{st.session_state.rol_usuario}**")
     mensaje = st.chat_input("Escribe tu pregunta aquí...")
 
     if mensaje:
@@ -95,8 +116,8 @@ elif st.session_state.estado == "chatbot":
             respuesta = obtener_respuesta(mensaje)
 
         st.session_state.chat.append(("Bot", respuesta))
-
-    #Mostrar historial de conversaciones
+    
+    #Mostrar el historial de conversaciones
     st.markdown("--------")
     for remitente, texto in st.session_state.chat:
         if remitente == "Tú":
